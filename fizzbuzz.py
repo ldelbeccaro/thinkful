00b0046384fd871833606e8e8f834f00cab3e46e
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2014-10-30 02:32:22 +0000

    First v2 commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,26 @@
-n = 100
+import sys
+
+args = [sys.argv for sys.argv in sys.argv[1:]]
+list = []
+
+for sys.argv in args:
+  try:
+    num = int(sys.argv)
+  except ValueError:
+    continue
+  else:
+    list.append(num)
+if len(list) > 0:
+  n = list[0]
+else:
+  while True:
+    try:
+      n = raw_input('Please enter an interger: ')
+      n = int(n)
+      break
+    except ValueError:
+      print 'Not an integer! Please try again.'
+
 print 'Fizz buzz counting up to', n
 for i in range(1, n+1):
   if i % 15 == 0:
