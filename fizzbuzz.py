c26d8b0be077b507bfbc63045b8f8d38bf1c87fa
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2014-10-30 00:19:08 +0000

    First commit

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+n = 100
+print 'Fizz buzz counting up to', n
+for i in range(1, n+1):
+  if i % 15 == 0:
+    print 'fizzbuzz'
+  elif i % 3 == 0:
+    print 'fizz'
+  elif i % 5 == 0:
+    print 'buzz'
+  else:
+    print i
